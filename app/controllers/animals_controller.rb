efb369415d2379f904c3672e89c6b424e959aae6
Erick Rivera <yomellamoerick.771@gmail.com>
2023-06-28 19:55:06 -0400

    views modified

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -65,6 +65,6 @@ class AnimalsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def animal_params
-      params.require(:animal).permit(:name)
+      params.require(:animal).permit(:name, :farmer_id)
     end
 end
---
+++ playground.rb
@@ -0,0 +1,7 @@
+Farmer.create(name: "Cletus", age: "33")
+Farmer.create(name: "Eileen", age: "27")
+
+#Esto puede iterarse con ciclos each
+Animal.find(1).farmer
+
+Farmer.find(2).animals
