f2081d1f21cfc9d12dad05676d24e3675bf7a695
Erick Rivera <yomellamoerick.771@gmail.com>
2023-06-28 21:47:17 -0400

    cascade destroy added

--- app/models/farmer.rb
+++ app/models/farmer.rb
@@ -1,4 +1,4 @@
 class Farmer < ApplicationRecord
-    has_many :animals #Si está escrito en plural es que tiene muchos
+    has_many :animals, dependent: :destroy #Si está escrito en plural es que tiene muchos, noo se van a morir los animales pipipi
     belongs_to :mainfarm #Singular pertenece solo a uno
 end
--- app/models/mainfarm.rb
+++ app/models/mainfarm.rb
@@ -1,5 +1,5 @@
 class Mainfarm < ApplicationRecord
-    has_many :farmers #En plural tiene muchos
+    has_many :farmers, dependent: :destroy #En plural tiene muchos. Con el dependent autorizamos la destrucción
 
     accepts_nested_attributes_for :farmers, allow_destroy: true #Con esto pueden eliminarse en cascada!!
 end
